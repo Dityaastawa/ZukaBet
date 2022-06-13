9156f126d09eedf432020582a40a1a57c26715de
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-06-13 15:27:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
-global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
-global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
+global.lolkey = 'bca5b104e231d4708ce6dfa8' //biar mudah ngegantinya semisal apikeynya expired:v
+global.zenzkey = '0a10a5cdcb' //ganti jadi apikey lu kalau expired
 global.wm = 'Ditya Astawa'
 global.watermark = wm
 global.wm2 = '                     「 Zuka Beta あ⁩ 」'
